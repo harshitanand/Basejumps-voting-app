47da243197ab267d94454dd35dc287881d038aa5
harshitanand <harshitanand94@gmail.com>
2015-12-31 03:21:03 +0530

    Made changes to the question poling layout

--- client/app/questions/question/question.controller.coffee
+++ client/app/questions/question/question.controller.coffee
@@ -1,5 +1,33 @@
 'use strict'
 
 angular.module 'project1App'
-.controller 'QuestionCtrl', ($scope) ->
-  $scope.message = 'Hello'
+.controller 'QuestionCtrl', ($scope, $routeParams, $http) ->
+  
+  $scope.data = []
+  $scope.canvote = false
+  $('#addoption').hide()
+  
+  endpoint = '/api/things/'+$routeParams.id
+  $http.get(endpoint).success (thing) ->
+    $scope.data = thing.data
+    if $scope?.data?.options.length isnt 0
+      $scope.canvote = true
+
+  $scope.moreoption = ->
+    $('#addoption').toggle(1000)
+    return false
+
+  $scope.addoption = ->
+    option =
+      name: $scope.option
+      votes: 0
+    $scope.canvote = true
+    $scope.data.options.push(option)
+
+  $scope.vote = ->
+    choice = $("input[name=ans]:checked").val()
+    $scope.data.options.forEach (option)->
+      if option.name is choice
+        option.votes += 1
+    $http.put endpoint,$scope.data
+
--- server/api/thing/thing.controller.js
+++ server/api/thing/thing.controller.js
@@ -31,19 +31,21 @@ exports.show = function(req, res) {
 
 // Creates a new thing in the DB.
 exports.create = function(req, res) {
-  Thing.create(req.body, function(err, thing) {
-    if(err) { return handleError(res, err); }
-    return res.status(201).json(thing);
-  });
+  if (req.body !== {}){
+    Thing.create(req.body, function(err, thing) {
+      if(err) { return handleError(res, err); }
+      return res.status(201).json(thing);
+    });
+  }
 };
 
 // Updates an existing thing in the DB.
 exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
+  //if(req.body._id) { delete req.body._id; }
   Thing.findById(req.params.id, function (err, thing) {
     if (err) { return handleError(res, err); }
     if(!thing) { return res.status(404).send('Not Found'); }
-    var updated = _.merge(thing, req.body);
+    var updated = _.extend(thing, req.body);
     updated.save(function (err) {
       if (err) { return handleError(res, err); }
       return res.status(200).json(thing);
